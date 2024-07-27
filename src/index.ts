165c63b96e873d1b0045b5a3d926a5599168d50d
XiaoXi <admin@soraharu.com>
2024-07-27 10:40:45 +0800

    chore: 示例代码中不需要异步标记的方法

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import * as extensionConfig from '../extension.json';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function activate(status?: 'onStartupFinished', arg?: string): void {}
 
-export async function about(): Promise<void> {
+export function about(): void {
 	eda.sys_MessageBox.showInformationMessage(
 		eda.sys_I18n.text('EasyEDA extension SDK v', undefined, undefined, extensionConfig.version),
 		eda.sys_I18n.text('About'),
