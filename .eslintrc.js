8781dd41b9391a6bd13ff9ac0d364b6c3ea86664
XiaoXi <admin@soraharu.com>
2024-08-14 16:26:10 +0800

    chore: 整理 ESLint 环境

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,9 +4,7 @@ module.exports = {
 	ignorePatterns: ['/build/dist/', '/coverage/', '/dist/', '/node_modules/', '/.eslintcache', 'debug.log'],
 	env: {
 		browser: true,
-		es2017: true,
 	},
-	globals: {},
 	rules: {
 		'no-param-reassign': 'off',
 		'max-params': 'off',
